da88564bae82c59340e02a134d8c128c1c65b5d3
Emily Shepperd <nshepperd@gmail.com>
2025-07-23 01:21:44 +1000

    fix broken monkeypatching with newer jax

--- jaxtorch/_util.py
+++ jaxtorch/_util.py
@@ -3,13 +3,29 @@ import jaxlib
 
 def ArrayTypes():
     array_types = []
-    if hasattr(jaxlib.xla_extension, 'ArrayImpl'):
-        array_types.append(jaxlib.xla_extension.ArrayImpl)
-    if hasattr(jaxlib.xla_extension, 'DeviceArrayBase'):
-        array_types.append(jaxlib.xla_extension.DeviceArrayBase)
-    if hasattr(jax.interpreters.xla, 'DeviceArray'):
-        array_types.append(jax.interpreters.xla.DeviceArray)
-    if hasattr(jax, 'Array'):
-        array_types.append(jax.Array)
+    try:
+        from jaxlib.xla_extension import ArrayImpl
+        array_types.append(ArrayImpl)
+    except ImportError:
+        pass
+    
+    try:
+        from jaxlib.xla_extension import DeviceArrayBase
+        array_types.append(DeviceArrayBase)
+    except ImportError:
+        pass
+    
+    try:
+        from jax.interpreters.xla import DeviceArray
+        array_types.append(DeviceArray)
+    except ImportError:
+        pass
+    
+    try:
+        from jax import Array
+        array_types.append(Array)
+    except ImportError:
+        pass
+
     return tuple(array_types)
 ArrayTypes = ArrayTypes()
